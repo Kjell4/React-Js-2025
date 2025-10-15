2f43cb077bdf8d319aefcbc7cd87db3800f3084b
Kjell4 <ilias.bekturgan@mail.ru>
2025-10-15 22:58:11 +0500

    search field and clear button added

--- project/react-project/src/Movies/MoviesList.jsx
+++ project/react-project/src/Movies/MoviesList.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useActionState, useState } from "react";
 import MovieCard from "../Card/MovieCard";
 import "./MoviesList.css";
 
 function MoviesList() {
   const [movies, setMovies] = useState([]);
+  const [filterQuery, setFilterQuery] = useState("");
 
   const fetchMovies = async () => {
 
@@ -15,12 +16,30 @@ function MoviesList() {
 
   };
 
+  const filteredMovies = movies.filter((movie) =>
+    movie["#TITLE"]?.toLowerCase().includes(filterQuery.toLowerCase())
+  );
+  const clearSearch = () => setFilterQuery("");
+
   return (
     <div className="movies-list-container">
+
+       <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search"
+          value={filterQuery}
+          onChange={(e) => setFilterQuery(e.target.value)}
+        />
+
+          <button className="clear-btn" onClick={clearSearch}>Clear</button>
+
+      </div>
+
       <button className="load-button" onClick={fetchMovies}>Load movies</button>
 
       <ul className="movies-list">
-        {movies.map((movie, index) => (
+        {filteredMovies.map((movie, index) => (
           <li className="movies-item" key={index}>
             <MovieCard movie={movie} />
           </li>
