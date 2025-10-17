be12e1fa45b4e84297dcf71cb8f4e951ee8d2ded
Kjell4 <ilias.bekturgan@mail.ru>
2025-10-18 00:47:16 +0500

    useEffect added

--- project/react-project/src/Movies/MoviesList.jsx
+++ project/react-project/src/Movies/MoviesList.jsx
@@ -1,4 +1,4 @@
-import React, { useActionState, useState } from "react";
+import React, { useState, useEffect } from "react";
 import MovieCard from "../Card/MovieCard";
 import "./MoviesList.css";
 
@@ -6,37 +6,36 @@ function MoviesList() {
   const [movies, setMovies] = useState([]);
   const [filterQuery, setFilterQuery] = useState("");
 
-  const fetchMovies = async () => {
-
+  
+  useEffect(() => {
+    const fetchMovies = async () => {
       const response = await fetch(
         "https://imdb.iamidiotareyoutoo.com/search?q=Marvel"
       );
       const data = await response.json();
       setMovies(data.description);
+    };
 
-  };
+    fetchMovies();
+  }, []);
 
   const filteredMovies = movies.filter((movie) =>
     movie["#TITLE"]?.toLowerCase().includes(filterQuery.toLowerCase())
   );
+
   const clearSearch = () => setFilterQuery("");
 
   return (
     <div className="movies-list-container">
-
-       <div className="search-bar">
+      <div className="search-bar">
         <input
           type="text"
           placeholder="Search"
           value={filterQuery}
           onChange={(e) => setFilterQuery(e.target.value)}
         />
-
-          <button className="clear-btn" onClick={clearSearch}>Clear</button>
-
+        <button className="clear-btn" onClick={clearSearch}>Clear</button>
       </div>
-
-      <button className="load-button" onClick={fetchMovies}>Load movies</button>
 
       <ul className="movies-list">
         {filteredMovies.map((movie, index) => (
